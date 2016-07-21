e5945fb3f7cbd71310b2bc2a7f4d70363dcef453
Chen Guo <chen.guo.0625@gmail.com>
2016-07-21 11:49:46 +0000

    front: search for actual search bar string

--- front/static/js/actions/searchActions.js
+++ front/static/js/actions/searchActions.js
@@ -1,29 +1,29 @@
+import qs from 'querystring';
+
 const getResults = (searchTerms, dataSource) => {
-	let searchEndpoint = '/search?qstr=hello'
-	return (dispatch) => {
-		console.log(searchTerms)
-		fetch(searchEndpoint, {
-      		method: 'GET'
-    	})
-		.then(response => { 
-			console.log(response)
-			if (response.status === 200) {
-				return response.json();
-			}
-			else {
-				if (browserSupportsLocalStorage()) {
-					localStorage.clear();
-				}
-				console.log(response)
-			}
-		})
-		.then(json => {
-			dispatch({
-				type: "RECEIVE_SEARCH_RESULTS",
-				results: json
-			})
-		})	
-	}
+  let queryString = qs.stringify({qstr: searchTerms})
+  let searchEndpoint = '/search?' + queryString
+  return (dispatch) => {
+    console.log(searchTerms)
+    fetch(searchEndpoint, {
+      method: 'GET'
+    }).then(response => {
+      console.log(response)
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        if (browserSupportsLocalStorage()) {
+          localStorage.clear();
+        }
+        console.log(response)
+      }
+    }).then(json => {
+      dispatch({
+        type: "RECEIVE_SEARCH_RESULTS",
+        results: json
+      })
+    })
+  }
 }
 
 export default getResults
