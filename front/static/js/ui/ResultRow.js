5cf84e6c721f8c837ee3386402605dd5d3ad384d
Felix Zhu <felix.czhu@gmail.com>
2016-07-20 23:12:57 -0700

    add manual chips

--- front/static/js/ui/ResultRow.js
+++ front/static/js/ui/ResultRow.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Chip from 'material-ui/Chip';
 import Divider from 'material-ui/Divider';
 
 let styles = {
@@ -21,17 +23,34 @@ let styles = {
   }
 }
 
-const ResultRow = (props) => (
-  <a target="_blank" href={props.url}>
-    <Card style={styles.resultCard} >
-      <CardHeader title={props.description} subtitle={props.title} />
-      <Divider />
-      <CardText style={styles.description}>
-        {props.url}
-      </CardText>
-    </Card>
-  </a>
-);
-
+const ResultRow = (props) => {
+  let manualTags = props.manual_tags.length > 0 ?
+    props.manual_tags.map( (tag, i) => (
+      <Chip onRequestDelete={(event) => console.log('deleted')}
+            onTouchTap={(event) => console.log('clicked')}
+            style={styles.chip}
+            key={i} >
+        {tag}
+      </Chip>
+    )) :
+    null
+  return (
+    <a target="_blank" href={props.url}>
+      <Card style={styles.resultCard} >
+        <CardHeader title={props.description} 
+                    subtitle={props.title} 
+                    showExpandableButton={true} 
+                    actAsExpander={true}/>
+        <Divider />
+        <CardText style={styles.description}>
+          {props.keywords.join(', ')}
+        </CardText>
+        <CardText expandable={true}>
+          {manualTags}
+        </CardText>
+      </Card>
+    </a>
+  );
+}
 
 export default ResultRow;
