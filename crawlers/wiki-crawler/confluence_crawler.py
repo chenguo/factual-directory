2fcc99c4a3ee2f431676b1a3600174b5d51f7885
Felix Zhu <felix.czhu@gmail.com>
2016-07-21 22:52:34 -0700

    add debug prints

--- crawlers/wiki-crawler/confluence_crawler.py
+++ crawlers/wiki-crawler/confluence_crawler.py
@@ -11,6 +11,7 @@ SEARCH_PARAMS = {
   'expand': 'space,body.export_view'
 }
 DEFAULT_KEYWORDS = ['confluence', 'wiki']
+SEEN = set()
 
 def do_parse():
   index_buffer = []
@@ -19,6 +20,7 @@ def do_parse():
     index_buffer.append(index)
     if len(index_buffer) > 1000:
       responses.append(requests.post(INDEX_API, json=index_buffer))
+      print responses[-1].status_code, responses[-1].text
       index_buffer = []
   responses.append(requests.post(INDEX_API, json=index_buffer))
   return responses
@@ -40,9 +42,13 @@ def get_indexes():
 
 def make_index(json):
   corpus = Soup(json['body']['export_view']['value']).text.split()
+  url = json['_links']['webui']
+  if url in SEEN:
+    print url
+  SEEN.add(url)
   return {
-    'id': 'confluence:' + json['title'],
-    'url': HOST + json['_links']['webui'],
+    'id': 'confluence:' + url,
+    'url': HOST + url,
     'keywords': [json['space']['key'], json['space']['name']] + DEFAULT_KEYWORDS,
     'title': json['title'],
     'description': ' '.join(corpus[:20]),
